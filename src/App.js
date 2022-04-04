6e5d15d0e44ee7aa664f5332941de994490483c2
nort4n <trikkster@gmail.com>
2022-04-04 10:28:58 +0200

    added hello world component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import { ALongComponent } from "./ALongComponent";
+import HelloWorld from "./HelloWorld";
 
 function App() {
-   return (
-      <div className="App">
-         <header className="App-header">
-            <ALongComponent />
-            <p>Hello Devhaus campers!</p>
-            <p>Cold as hell today</p>
-         </header>
-         <h3>Hello!dasdasda dasdada</h3>
-         <p>New line 😥</p>
-      </div>
-   );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ALongComponent />
+        <p>Hello Devhaus campers!</p>
+        <p>Cold as hell today</p>
+      </header>
+      <h3>Hello!dasdasda dasdada</h3>
+      <p>New line 😥</p>
+      <HelloWorld />
+    </div>
+  );
 }
 
 export default App;
