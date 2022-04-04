3b533f8555394a30ac89c14afb88d04ab57005cb
Tanous Atallah <tanous.atallah@gmail.com>
2022-04-04 10:30:37 +0200

    change h2 text

--- src/TonyComponent.jsx
+++ src/TonyComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function TonyComponent(){
     return(
         <div>
-            <h2>T0ny86 Component</h2>
+            <h2>T0ny86 Component, hi</h2>
         </div>
     );
 }
