bfadcdca5c410de91e4a5b11dcab139340ecc197
JonathanWagne <jonathan_wagner@hotmail.de>
2022-04-04 10:22:00 +0200

    Add JonathansComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { ALongComponent } from "./ALongComponent";
+import { JonathansComponent } from "./JonathansComponent.jsx"
 
 function App() {
    return (
       <div className="App">
          <header className="App-header">
             <ALongComponent />
+            <JonathansComponent/>
          </header>
       </div>
    );
---
+++ src/JonathansComponent.jsx
@@ -0,0 +1,5 @@
+import react from "react";
+
+export function JonathansComponent() {
+  return <p>Look at this Text</p>;
+}
