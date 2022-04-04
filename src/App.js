ef1e24721ffd48f99dd5dba8de28d3b6fa0f2192
Dean Wallflower <dean@dean.fritz.box>
2022-04-04 10:16:16 +0200

    added a component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { ALongComponent } from "./ALongComponent";
+import { DeansComp } from "./DeansComp";
 
 function App() {
-   return (
-      <div className="App">
-         <header className="App-header">
-            <ALongComponent />
-         </header>
-      </div>
-   );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ALongComponent />
+        <DeansComp />
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/DeansComp.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function DeansComp() {
+  return (
+    <h1>
+      <em>
+        <strong>wozzup</strong>
+      </em>
+    </h1>
+  );
+}
+
+export default DeansComp;
