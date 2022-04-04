295989ea4f333685d9bac41e92bf1347a46b8440
Dean Wallflower <dean@dean.fritz.box>
2022-04-04 10:21:05 +0200

    deans comp

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { DeansComp } from "./DeansComp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ALongComponent />
-        <DeansComp />
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <ALongComponent />
+          <p>Hello Devhaus campers!</p>
+          <p>Cold as hell today</p>
+        </header>
+        <h3>Hello!dasdasda dasdada</h3>
+        <p>New line 😥</p>
+      </div>
+      <DeansComp />
+    </>
   );
 }
 
